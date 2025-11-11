8a8b44a9ad150f8da1991d56e3d7359b351ff04a
Bernt Isak Wærstad <berntisak@gmail.com>
2025-11-11 23:12:30 +0100

    Key variable update

--- src/_data/lineup.js
+++ src/_data/lineup.js
@@ -5,7 +5,7 @@ const { createClient } = require('@supabase/supabase-js');
 module.exports = async function() {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY
   );
 
   const { data: artists, error } = await supabase
